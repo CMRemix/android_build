1f3e79dff79c2122261ee29e629ede6e265d73c4
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2017-02-24 21:13:11 +0100

    Multiple optimizations I've been adding and removing polly optimizations for a while and i've not noticed any gains or loss through synthetics benchmarks or simply by judging how "smooth" android is in a whole (not a scientific test). I don't see the point of keeping it if it's not a win situation. I believe we would need LTO to release the whole power of polly, which we simply can't do on a lot of binaries. Also remove LTO. Move around rs clang version into clang version definiton file because it makes sense? Use cortex-a57 optimization on cortexa53.a57 target.
    # Conflicts:
    #	core/clang/config.mk
    #	core/clang/versions.mk
    #	core/combo/arch/arm64/armv8-a.mk

--- core/combo/arch/arm64/armv8-a.mk
+++ core/combo/arch/arm64/armv8-a.mk
@@ -1,5 +1,33 @@
-ifneq (,$(filter cortex-a53,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
-	arch_variant_cflags := -mcpu=cortex-a53
-else
-	arch_variant_cflags :=
+APPLY_A53_ERRATA_FIXES :=
+arch_variant_cflags :=
+
+ifneq ($(filter kryo,$(TARGET_2ND_CPU_VARIANT)),)
+	arch_variant_cflags := -mcpu=cortex-a57
 endif
+
+ifneq ($(filter cortex-a53,$(TARGET_2ND_CPU_VARIANT)),)
+	arch_variant_cflags := -mcpu=cortex-a53
+	APPLY_A53_ERRATA_FIXES := true
+endif
+
+ifneq ($(filter cortex-a53.a57,$(TARGET_2ND_CPU_VARIANT)),)
+        arch_variant_cflags := -mcpu=cortex-a57
+        APPLY_A53_ERRATA_FIXES := true
+endif
+
+ifneq ($(strip $(TARGET_IS_CORTEX-A53)),)
+	APPLY_A53_ERRATA_FIXES := $(TARGET_IS_CORTEX-A53)
+endif
+
+ifeq ($(APPLY_A53_ERRATA_FIXES),true)
+	arch_variant_cflags  += -mfix-cortex-a53-835769
+	arch_variant_ldflags := -Wl,--fix-cortex-a53-843419
+	arch_variant_ldflags += -Wl,--fix-cortex-a53-835769
+else
+	arch_variant_cflags  += -mno-fix-cortex-a53-835769
+	arch_variant_ldflags := -Wl,--no-fix-cortex-a53-843419
+	arch_variant_ldflags += -Wl,--no-fix-cortex-a53-835769
+	RS_DISABLE_A53_WORKAROUND := true
+endif
+
+APPLY_A53_ERRATA_FIXES :=
